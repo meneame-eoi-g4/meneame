4bd406b92e94f964f6bf9cfe52e783855809850e
zarby_developer <zarby@DESKTOP-BFAMI13.localdomain>
2021-06-03 09:45:52 +0200

    diseño de muro de comentarios y elementos

---
+++ components/comentsWall.vue
@@ -0,0 +1,37 @@
+<template>
+ <div class="container-wrap">
+      <div class="columns is-gapless is-flex is-multiline">
+        <comentitem
+          v-for="item in coments"
+          :value="item"
+          :key="item._id"
+        />
+      </div>
+    </div>
+</template>
+<script>
+import comentItem from "@/components/partials/comentitem";
+
+
+
+export default {
+  name: "mainWall",
+  components: {
+    comentItem
+    
+    
+  },
+  props: {
+    comments: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }
+  },
+  computed: {
+   
+    
+  }
+};
+</script>
---
+++ components/partials/comentItem.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img v-if="value.author.img" src=value.author.img alt="Placeholder image">
+          <img v-if="!value.author.img" src='https://img2.freepng.es/20180616/sxr/kisspng-avatar-computer-icons-avatar-icon-5b254abb7cf344.7556131215291706195118.jpg' alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">{{value.author.name}}</p>
+        <p class="subtitle is-6">@{{value.author.name}}</p>
+      </div>
+    </div>
+
+    <div class="content">
+     {{value.content}}
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br>
+      <time datetime="2016-1-1">{{value.date}}</time>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: "comentItem",
+  props: {
+    value: Object,
+  }
+}
+</script>
